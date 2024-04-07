0e3107784066cc069dd363dd2c59cd13082dbc35
Andres Marulanda <80536992+AndresMarulanda10@users.noreply.github.com>
2024-04-07 05:40:58 +0000

    Structuring of view model, querysets and serializer

--- library_system/views.py
+++ library_system/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Book, User, Loan
+from .serializers import BookSerializer, UserSerializer, LoanSerializer
 
-# Create your views here.
+class BookList(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class UserList(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class LoanList(generics.ListCreateAPIView):
+    queryset = Loan.objects.all()
+    serializer_class = LoanSerializer
+
+class LoanDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Loan.objects.all()
+    serializer_class = LoanSerializer
